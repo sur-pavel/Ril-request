d243e20daa0715588428432b9c9d8dbe33578f6e
sur-pavel <sur-pavel@mail.ru>
2015-04-29 11:53:28 +0300

    Update XposedMod.java

--- src/com/sur-pavel/ril-request/XposedMod.java
+++ src/com/sur-pavel/ril-request/XposedMod.java
@@ -4,6 +4,7 @@ import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+import android.widget.TextView;
 
 public class CatchOem implements IXposedHookLoadPackage {
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
@@ -14,8 +15,9 @@ public class CatchOem implements IXposedHookLoadPackage {
                           "invokeOemRilRequestRaw", "android.os.Bundle", new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-	XposedBridge.log("LocalRILRequest:" + localRILRequest);
-        XposedBridge.log("ParamArrayOfByte :" + paramArrayOfByte);  }
+	TextView tv = (TextView) param.thisObject;
+	XposedBridge.log("invokeOemRilRequestRaw arguments:" + tv);
+          }
         });
     }
 }
